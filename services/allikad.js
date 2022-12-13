d968abde06be5a77243df48dc0af209d040436e8
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-12-13 16:29:40 +0200

    Update allikad.js

--- services/allikad.js
+++ services/allikad.js
@@ -2,7 +2,8 @@ const db = require('./db')
 const helper = require('../helper')
 const config = require('../config')
 
-async function getMultiple(page = 1){
+async function getMultiple(page = 1) {
+  console.log(config)
   const offset = helper.getOffset(page, config.listPerPage)
   const rows = await db.query(
     `SELECT id, allikas, kood, prioriteetPerenimi, prioriteetEesnimi, prioriteetIsanimi, prioriteetEmanimi, prioriteetSünd, prioriteetSurm, prioriteetKirje 
